d9014931563cb166a73bdaa09514330b69edec14
Aquiba Benarroch <aquibabenarroch@gmail.com>
2021-07-08 13:55:24 -0400

    test filters

--- qualifier/tests/test_qualifier.py
+++ qualifier/tests/test_qualifier.py
@@ -25,21 +25,18 @@ def test_calculate_loan_to_value_ratio():
     assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
 
 def test_filters():
+    "Testing loan filters"
+
     bank_list = fileio.load_csv(Path('./data/daily_rate_sheet.csv'))
+    
     current_credit_score = credit_score.filter_credit_score(750, bank_list)
-    assert isinstance(current_credit_score, int)
-    debt = 1500.0
-    assert isinstance(debt, float)
-    income = 4000.0
-    assert isinstance(income, float)
-    loan = 210000.0
-    assert isinstance(loan, float)
-    home_value = 250000.0
-    assert isinstance(home_value, float)
+    assert ['Bank of Big - Premier Option', '300000', '0.85', '0.47', '740', '3.6'] in current_credit_score
 
-    monthly_debt_ratio = 0.375
-    assert isinstance(monthly_debt_ratio, float)
+    dti = debt_to_income.filter_debt_to_income(0.4, bank_list)
+    assert ['Bank of Big - Premier Option', '300000', '0.85', '0.47', '740', '3.6'] in dti
 
-    loan_to_value_ratio = 0.84
-    assert isinstance(loan_to_value_ratio, float)
+    ltv = loan_to_value.filter_loan_to_value(0.6, bank_list)
+    assert ['Bank of Big - Premier Option', '300000', '0.85', '0.47', '740', '3.6'] in ltv
 
+    loan_size = max_loan_size.filter_max_loan_size(20000, bank_list)
+    assert ['Bank of Big - Premier Option', '300000', '0.85', '0.47', '740', '3.6'] in ltv
