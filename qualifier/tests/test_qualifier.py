d33d10e11b2e9579830a032129078383466c44b9
Aquiba Benarroch <aquibabenarroch@gmail.com>
2021-07-09 14:31:41 -0400

    Added documentation

--- qualifier/tests/test_qualifier.py
+++ qualifier/tests/test_qualifier.py
@@ -15,20 +15,55 @@ from qualifier.filters import loan_to_value
 from qualifier.filters import max_loan_size
 
 def test_save_csv():
-    # Use Path from pathlib to output the test csv to ./data/output/qualifying_loans.csv
+    '''Test saving qulifying loans to CSV file'''
+
     assert Path("/Users/aquiba/Fintech-Workspace/Module_2/bootcamp_challenge2/qualifier/data_qualifying_loans.csv").exists()
 
 def test_calculate_monthly_debt_ratio():
+    '''Test the monthly debt ratio calculation
+    
+    Args:
+        Debt outstanding
+        Income
+
+    Test:
+        Debt / income
+    '''
+
     assert calculators.calculate_monthly_debt_ratio(1500, 4000) == 0.375
 
 def test_calculate_loan_to_value_ratio():
+    '''Test the monthly debt ratio calculation
+    
+    Args: 
+        Loan requested
+        Home value
+
+    Test:
+        Loan / home value 
+    '''
     assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
 
 def test_filters():
-    "Testing loan filters"
+    '''Test of loan filters
+    
+    Arg:
+        Credit score
+        Debt to income
+        Loan to value
+        Max loan size
+        Bank list
+    
+    Tests:
+        Loan meets credit score filter
+        Loan meets debt to income filter
+        Loan meets loan to value filter
+        Loan meets max loan size filter
+    
+    '''
 
     bank_list = fileio.load_csv(Path('./data/daily_rate_sheet.csv'))
-    
+
     current_credit_score = credit_score.filter_credit_score(750, bank_list)
     assert ['Bank of Big - Premier Option', '300000', '0.85', '0.47', '740', '3.6'] in current_credit_score
 
